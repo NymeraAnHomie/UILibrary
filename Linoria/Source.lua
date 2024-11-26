662b59d1a683630b69643de2b8b69da1c4b10854
NymeraAnHomie <frostiesnowman439@gmail.com>
2024-11-26 16:24:30 +0800

    Update Source.lua

--- Linoria/Source.lua
+++ Linoria/Source.lua
@@ -1,6 +1,6 @@
 local InputService = game:GetService('UserInputService');
 local TextService = game:GetService('TextService');
-local CoreGui = game:GetService('CoreGui');
+local CoreGui = cloneref(game:GetService('CoreGui')); -- UI Maker Pls learn cloneref
 local Teams = game:GetService('Teams');
 local Players = game:GetService('Players');
 local RunService = game:GetService('RunService')
