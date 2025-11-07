c30bfbdd6b9792fffdf7cb4dd8b3519ff30ba9a3
Nymera <frostiesnowman439@gmail.com>
2025-11-07 16:26:44 +0800

    Update Source.lua

--- Octohook/Source.lua
+++ Octohook/Source.lua
@@ -2616,7 +2616,9 @@
                 Side = properties.side or properties.Side or "Left";
 
                 -- Fill settings 
-                Size = properties.size or properties.Size or nil;
+                Size = (typeof(rawSize) == "number" and dim2(0, rawSize, 0, 0))
+                    or (typeof(rawSize) == "UDim2" and rawSize)
+                    or nil,
                 
                 -- Other
                 Items = {};
@@ -2626,7 +2628,7 @@
                 Items.Outline = Library:Create( "Frame" , {
                     Parent = self.Items.Column;
                     Name = "\0";
-                    Size = Cfg.Size or dim2(0, 100, 0, 0);
+                    Size = dim2(0, 100, 0, 0);
                     BorderColor3 = rgb(0, 0, 0);
                     BorderSizePixel = 0;
                     AutomaticSize = Enum.AutomaticSize.Y;
